374e924ba3f5b33c08d4181f96156a9c0f1f0420
PakornPK <sweetie_jimmie@hotmail.com>
2019-05-11 00:22:44 +0700

    :)

---
+++ exercise2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std; 
+
+int main(){
+    int num1,num2; 
+    cout << "Sample Output :" << endl; 
+    cout << "Print the sum of two number :" ; 
+    cin >> num1 >>  num2; 
+    cout << "-------------------------------" << endl; 
+    cout << "The sum of " << num1 << " and " << num2 << " is : " << num1+num2 << endl; 
+    return 0; 
+}
